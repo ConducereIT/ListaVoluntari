d1c06f2268b1e3b022dd7c5416cbd54fe88f7ca6
NameIsGabe <gabethefluffydoggo@gmail.com>
2023-10-19 21:44:01 +0300

    please ;-;

--- client/src/ListaVoluntari.js
+++ client/src/ListaVoluntari.js
@@ -1,10 +1,11 @@
 export default function ListaVoluntari() {
   return (
-    <>
+      <>
       <h1>Dulgheriu Bogdan Gabriel - dulgheriubogdan</h1>
       <h1>Calin Silvian Petre - xRankk</h1>
       <h1>Cristi Miloiu - cristim67</h1>
-      <h1>Muie Ionel<h1/>
-    </>
+      <h1>Muie Ionel</h1>
+      <h1>Malacescu Gabriel Tiberiu - NameIsGabe</h1>
+      </>
   );
 }
