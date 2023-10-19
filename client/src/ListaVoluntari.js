db1e46e2a6e0d3701582f63e3c50a20574770e6e
Ciprau <ciprianrm12@gmail.com>
2023-10-19 21:50:20 +0300

    mesaj

--- client/src/ListaVoluntari.js
+++ client/src/ListaVoluntari.js
@@ -3,8 +3,9 @@ export default function ListaVoluntari() {
     <>
       <h1>Dulgheriu Bogdan Gabriel - dulgheriubogdan</h1>
       <h1>Calin Silvian Petre - xRankk</h1>
+      <h1>Cazan Florin-Ciprian - Ciprau</h1>
       <h1>Cristi Miloiu - cristim67</h1>
-      <h1>Muie Ionel</h1>
+      <h1>Muie Ionel</h1> 
     </>
   );
 }
