4bb313ee821d91a5cd60956b14c8a350cbf1b2e5
ivannandrei <eusuntandrei9@gmail.com>
2023-10-19 21:42:23 +0300

    mi-am ad numele

--- client/src/ListaVoluntari.js
+++ client/src/ListaVoluntari.js
@@ -5,6 +5,8 @@ export default function ListaVoluntari() {
       <h1>Calin Silvian Petre - xRankk</h1>
       <h1>Cristi Miloiu - cristim67</h1>
       <h1>Muie Ionel</h1>
+      <h1>Ivan Andrei - ivannandrei</h1>
     </>
+  
   );
 }
